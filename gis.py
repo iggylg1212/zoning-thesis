c191dc6d8f7f343eca447521651c6d618ceea50a
iggylg1212 <iml2114@columbia.edu>
2022-01-05 19:07:33 +0000

    Changed File Writing

--- gis.py
+++ gis.py
@@ -170,29 +170,28 @@ import os.path as path
 
 # public_lands = pad_processor(1,'Marine')
 # for set in sets:
-#     for region in regions:
+#     for region in regions:pip
 #         public_lands = public_lands.append(pad_processor(region, set))
 
 # public_land = public_lands.to_crs('EPSG:4326')
 # public_lands.to_file('1data/gis/PAD/PAD_Concat/PAD_Concat.shp')
 
 # Concatentate
-# uas = read_shapefile('gis/gis/us_urb_area_1990/reprojection_urb_area_1990', 'EPSG:4326')
+public_lands = gpd.read_file(f'{S3_PATH}gis/gis/PAD/PAD_Concat/PAD_Concat.shp', crs='EPSG:4326')[['geometry']]
+water_wet = gpd.read_file(f'{S3_PATH}gis/gis/undev_land_cover/undev_land_cover.gpkg', crs='EPSG:4326')[['geometry']]
+elev = gpd.read_file(f'{S3_PATH}gis/gis/undev_land_cover/undev_land_cover.gpkg', crs='EPSG:4326')[['geometry']]
 
-water_wet = gpd.read_file(read_s3_file('gis/gis/undev_land_cover/undev_land_cover.gpkg'), crs='EPSG:4326')[['geometry']]
-print(water_wet)
-# elev = gpd.read_file(read_file('1data/gis/undev_slope/undev_slope.gpkg') )[['geometry']]
-# public_lands = gpd.read_file(read_file('1data/gis/PAD/PAD_Concat/PAD_Concat.shp'))[['geometry']]
-
-# concat = water_wet.append(elev, ignore_index=True).append(public_lands, ignore_index=True)
-
-# overlap_matrix = concat['geometry'].apply(lambda x: concat.overlaps(x)).values.astype(int)
-# n, ids = connected_components(overlap_matrix)
-
-# concat = gpd.GeoDataFrame({'geometry': concat['geometry'], 'group': ids})
-# res = concat.dissolve(by='group')
-
-# write_gpd('gis/gis/undev_concat/undev_concat.gpkg', uas, 'GPKG')
+concat = water_wet.append(elev, ignore_index=True).append(public_lands, ignore_index=True)
+print('Creating Overlap')
+overlap_matrix = concat['geometry'].apply(lambda x: concat.overlaps(x)).values.astype(int)
+n, ids = connected_components(overlap_matrix)
+print('Dissolving')
+concat = gpd.GeoDataFrame({'geometry': concat['geometry'], 'group': ids})
+res = concat.dissolve(by='group')
+print('Writing File')
+res.to_file('res.gpkg', driver = 'GPKG')
+S3FS.put('res.gpkg', f'{S3_PATH}gis/gis/undev_concat/undev_concat.gpkg')
+os.remove('res.gpkg')
 
 # # Find spatial intersection of UAs and undevelopable areas
 # undev = gpd.read_file('1data/gis/undev_concat/undev_concat.gpkg')
--- global_var.py
+++ global_var.py
@@ -1,40 +1,10 @@
 import boto3
 import shapefile
-import pandas as pd
-import geopandas as gpd
-import io
+import s3fs
+import os
 
 S3 = boto3.client("s3")
 S3_PATH_NAME = "thesis1212"
 S3_PATH = f"s3://{S3_PATH_NAME}/"
 
-def read_s3_file(key):
-    return io.BytesIO(S3.get_object(Bucket=S3_PATH_NAME, Key=key)['Body'].read())
-
-def read_shapefile(key, crs):
-    sf = shapefile.Reader(shp=io.BytesIO(S3.get_object(Bucket=S3_PATH_NAME, Key=f'{key}.shp')['Body'].read()),
-                              shx=io.BytesIO(S3.get_object(Bucket=S3_PATH_NAME, Key=f'{key}.shx')['Body'].read()),
-                              dbf=io.BytesIO(S3.get_object(Bucket=S3_PATH_NAME, Key=f'{key}.dbf')['Body'].read()),
-                              prj=io.BytesIO(S3.get_object(Bucket=S3_PATH_NAME, Key=f'{key}.prj')['Body'].read()),
-                              cpg=io.BytesIO(S3.get_object(Bucket=S3_PATH_NAME, Key=f'{key}.cpg')['Body'].read()))
-
-    fields = [x[0] for x in sf.fields][1:]
-    records = sf.records()
-    shps = [s for s in sf.shapes()]
-
-    #write into a dataframe
-    df = gpd.GeoDataFrame(pd.DataFrame(columns=fields, data=records).assign(geometry=shps), crs=crs)
-    
-    return df
-
-def write_csv(key, df):
-    csv_buf = io.StringIO()
-    df.to_csv(csv_buf, header=True, index=False)
-    csv_buf.seek(0)
-    S3.put_object(Bucket=S3_PATH_NAME, Body=csv_buf.getvalue(), Key=key)
-
-def write_gpd(key, df, driver):
-    gpkg_buf = io.BytesIO()
-    df.to_file(gpkg_buf, driver=driver)
-    gpkg_buf.seek(0)
-    S3.put_object(Bucket=S3_PATH_NAME, Body=gpkg_buf.getvalue(), Key=key)
+S3FS = s3fs.S3FileSystem()
