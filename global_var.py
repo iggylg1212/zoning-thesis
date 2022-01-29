c4faa840ce3ea029511540ba6ec7af22dbd2ffce
iggylg1212 <iml2114@columbia.edu>
2022-01-29 17:45:08 -0500

    updated nlp script

--- global_var.py
+++ global_var.py
@@ -1,6 +1,5 @@
 import boto3
 import s3fs
-import os
 
 S3 = boto3.client("s3")
 S3_PATH_NAME = "thesis1212"
--- nlp.py
+++ nlp.py
@@ -1,13 +1,14 @@
 from global_var import *
-import sqlite3
-from sqlite3 import Error
 from contextlib import closing
+from config import * 
 import math
 import csv
 import tqdm
+import pymysql
 import sys
 import os
 import pandas as pd
+import geopandas as gpd
 import numpy as np
 from multiprocessing import Pool, Array
 import re
@@ -15,12 +16,12 @@ import random
 from sentence_transformers import SentenceTransformer, losses, InputExample, util, evaluation
 from torch.utils.data import DataLoader
 
-def create_connection(db_file):
+def create_connection():
     conn = None
     try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
+        conn = pymysql.connect(host=HOST, user=USER, password=PASSWORD, database=DATABASE)
+    except:
+        print('Error')
 
     return conn
 
@@ -32,22 +33,24 @@ def create_row(conn, row):
                       year INTEGER NOT NULL, 
                       month INTEGER NOT NULL, 
                       day INTEGER NOT NULL, 
-                      zone_text TEXT NOT NULL 
+                      zone_text LONGTEXT NOT NULL,
+                      gisjoin TEXT,
+                      govtype TEXT,
+                      urbanized BOOL DEFAULT 0
                     ); ''')
     conn.commit()
-    cur.execute("INSERT INTO zoning (place, state, year, month, day, zone_text) values (?,?,?,?,?,?);", row)
+    cur.execute("INSERT INTO zoning (place, state, year, month, day, zone_text) values (%s,%s,%s,%s,%s,%s);", row)
     conn.commit()
 
-database = 'zoning_corpus.db'
-
-conn = create_connection(database)
+conn = create_connection()
 with conn:
   cur = conn.cursor()
-  done_list = set(cur.execute('SELECT place, state, year, month, day FROM zoning').fetchall())
+  cur.execute('SELECT place, state, year, month, day FROM zoning')
+  done_list = set(cur.fetchall())
 
 zoning_phrases = ['development', 'development code', 'zoning', 'architectural', 'infill', 'density', 'housing', 'land', 'growth areas', 'building', 'preservation', 'historic', 'land use', 'neighborhood', 'subdivision', 'planning', 'property maintenace']
 directory = S3FS.ls(f'{S3_PATH}nlp/csv/municode')
-tasks = directory[:int(.5*len(directory))] #random.sample(directory, k=3)
+tasks = directory #random.sample(directory, k=3)
 
 model_name = 'paraphrase-MiniLM-L3-v2'
 model_save_path = f'continue_training-{model_name}'
@@ -186,14 +189,126 @@ def loop(filename):
 
     zone_text = text_creator(zone_text)
 
-    conn = create_connection(database)
+    conn = create_connection()
     with conn:
       row = [muni, state, year, month, day, zone_text]
       create_row(conn, row)
 
 if __name__ == "__main__":
-  pool= Pool()
+  pool = Pool()
   for _ in tqdm.tqdm(pool.imap_unordered(loop, tasks), total=len(tasks)):
     pass
-  S3FS.put('zoning_corpus.db', f'{S3_PATH}nlp/database/zoning_corpus.db')
-  os.remove('zoning_corpus.db')
+
+# # Adding GISJOINs to zoning_corpus
+# us_state_to_abbrev = {"Alabama": "AL", "Alaska": "AK", "Arizona": "AZ", "Arkansas": "AR", "California": "CA", "Colorado": "CO", "Connecticut": "CT", "Delaware": "DE", "Florida": "FL", "Georgia": "GA", "Hawaii": "HI", "Idaho": "ID", "Illinois": "IL", "Indiana": "IN", "Iowa": "IA", "Kansas": "KS", "Kentucky": "KY", "Louisiana": "LA", "Maine": "ME", "Maryland": "MD", "Massachusetts": "MA", "Michigan": "MI", "Minnesota": "MN", "Mississippi": "MS", "Missouri": "MO", "Montana": "MT", "Nebraska": "NE", "Nevada": "NV", "New Hampshire": "NH", "New Jersey": "NJ", "New Mexico": "NM", "New York": "NY", "North Carolina": "NC", "North Dakota": "ND", "Ohio": "OH", "Oklahoma": "OK", "Oregon": "OR", "Pennsylvania": "PA", "Rhode Island": "RI", "South Carolina": "SC", "South Dakota": "SD", "Tennessee": "TN", "Texas": "TX", "Utah": "UT", "Vermont": "VT", "Virginia": "VA", "Washington": "WA", "West Virginia": "WV", "Wisconsin": "WI", "Wyoming": "WY", "District of Columbia": "DC", "American Samoa": "AS", "Guam": "GU", "Northern Mariana Islands": "MP", "Puerto Rico": "PR", "United States Minor Outlying Islands": "UM", "U.S. Virgin Islands": "VI"}
+#
+# county_code= pd.read_csv(f'{S3_PATH}nlp/csv/2010_population/nhgis0017_ds120_1990_county.csv', encoding = "ISO-8859-1")[['GISJOIN','STATE','ANPSADPI']]
+# place_code = pd.read_csv(f'{S3_PATH}nlp/csv/2010_population/nhgis0017_ds120_1990_place.csv', encoding = "ISO-8859-1")[['GISJOIN','STATE','PLACE']]
+# codes = pd.concat([place_code, county_code], axis=0)
+# codes['STATE'] = codes['STATE'].replace(us_state_to_abbrev)
+# codes['PLACE'] = codes['PLACE'].fillna('').apply(lambda x: ' '.join(x.split(' ')[:-1]))
+# codes['ANPSADPI'] = codes['ANPSADPI'].fillna('')
+#
+# place_list = str(set(codes[codes['PLACE']!='']['GISJOIN'])).replace('}',')').replace('{','(')
+# county_list = str(set(codes[codes['ANPSADPI']!='']['GISJOIN'])).replace('}',')').replace('{','(')
+#
+# conn = create_connection()
+# with conn:
+#   cur = conn.cursor()
+  # for id, row in codes[codes['ANPSADPI']!=''].iterrows():
+  #   county = row['ANPSADPI'].replace("'","''")
+  #   cur.execute(f"UPDATE zoning SET gisjoin = '{row['GISJOIN']}' WHERE place='{county}' AND state='{row['STATE']}'")
+  #   conn.commit()
+  # for id, row in codes[codes['PLACE']!=''].iterrows():
+  #   place = row["PLACE"].replace("'","''")
+  #   cur.execute(f"UPDATE zoning SET gisjoin = '{row['GISJOIN']}' WHERE place ='{place}' AND state='{row['STATE']}'")
+  #   conn.commit()
+  # len = len(codes[codes['PLACE']!=''])
+  # number = 0
+  # for id, row in codes[codes['PLACE']!=''].iterrows():
+  #   number+=1
+  #   print(round(number/len, 4))
+  #   place = row["PLACE"].replace("'","''")
+  #   cur.execute(f"UPDATE zoning SET gisjoin='{row['GISJOIN']}' WHERE gisjoin is NULL and place LIKE '%{place}%' AND state='{row['STATE']}'")
+  #   conn.commit()
+  # for id, row in codes[(codes['ANPSADPI']!='') & (codes['STATE']=='LA')].iterrows(): # Correcting 'police jury'
+  #   county = row['ANPSADPI'].replace("'","''")
+  #   cur.execute(f"UPDATE zoning SET gisjoin='{row['GISJOIN']}' WHERE gisjoin is NULL and place LIKE '%{county}%' AND state='LA'")
+  #   conn.commit()
+
+  # cur.execute('SELECT place, state FROM zoning WHERE gisjoin IS NULL')
+  # null_set = set(cur.fetchall())
+  # dataframe = pd.DataFrame(columns=['og_place','og_state'])
+  # for set in null_set:
+  #   dataframe = dataframe.append({'og_place':set[0],'og_state':set[1]}, ignore_index=True)
+  # dataframe.to_csv('broken.csv')
+
+  # corrected = pd.read_csv('broken.csv').fillna('')
+  # for id, row in corrected.iterrows():
+  #   og_place = row['og_place'].replace("'","''")
+  #   new_place = row['new_place'].replace("'","''")
+  #   cur.execute(f"UPDATE zoning SET place='{new_place}', state='{row['new_state']}' WHERE place='{og_place}' AND state='{row['og_state']}'")
+  #   conn.commit()
+
+  # len = len(codes[codes['ANPSADPI']!=''])+len(codes[codes['PLACE']!=''])
+  # number = 0 
+  # for id, row in codes[codes['ANPSADPI']!=''].iterrows():
+  #   number += 1
+  #   print(round(number/len,4))
+  #   county = row['ANPSADPI'].replace("'","''")
+  #   cur.execute(f"UPDATE zoning SET gisjoin = '{row['GISJOIN']}' WHERE gisjoin is NULL AND place='{county}' AND state='{row['STATE']}'")
+  #   conn.commit()
+  # for id, row in codes[codes['PLACE']!=''].iterrows():
+  #   number += 1
+  #   print(round(number/len,4))
+  #   place = row["PLACE"].replace("'","''")
+  #   cur.execute(f"UPDATE zoning SET gisjoin = '{row['GISJOIN']}' WHERE gisjoin is NULL AND place ='{place}' AND state='{row['STATE']}'")
+  #   conn.commit()
+
+  # cur.execute(f"UPDATE zoning SET govtype = 'county' WHERE gisjoin in {county_list}")
+  # cur.execute(f"UPDATE zoning SET govtype = 'place' WHERE gisjoin in {place_list}")
+  # conn.commit()
+
+# # Finding places and counties in urbanized areas
+# def import_shpfile(path):
+#   shp_files = ['.dbf','.prj','.sbn','.sbx','.shp','.shp.xml','.shx']
+#   for file in shp_files:
+#     S3FS.get(f'{S3_PATH}{path}{file}', f'temporary{file}')
+#   df = gpd.read_file('temporary.shp', crs='EPSG:4326')
+#   for file in shp_files:
+#     os.remove(f'temporary{file}')
+#   return df
+#
+# urbs = import_shpfile('nlp/gis/urb_area_1990/US_urb_area_1990').to_crs('EPSG:4326')
+#
+# county = import_shpfile('nlp/gis/county_1990/US_county_1990').to_crs('EPSG:4326')
+# county['source'] = 'county'
+# places = import_shpfile('nlp/gis/us_place_1990/US_place_1990').to_crs('EPSG:4326')
+# places['source'] = 'place'
+# govs = pd.concat([county, places])[['GISJOIN','source','geometry']]
+#
+# urb_gov = gpd.GeoDataFrame(columns=['GISJOIN','UACODE','source','geometry'], crs='EPSG:4326')
+# for row in list(range(len(urbs))):
+#     print(round(row/len(urbs),4))
+#     city = urbs.iloc[row][['UACODE','geometry']]
+#     govs['bool'] = govs.intersects(city['geometry'])
+#     govs['UACODE'] = city['UACODE']
+#     set = govs[govs['bool'] == True]
+#     urb_gov = pd.concat([urb_gov, set])
+#
+# urb_gov.to_file('urb_gov.gpkg')
+# S3FS.put('urb_gov.gpkg',f'{S3_PATH}nlp/gis/urb_gov_1990.gpkg')
+# os.remove('urb_gov.gpkg')
+
+# # Find urban entries
+# S3FS.get(f'{S3_PATH}nlp/gis/urb_gov_1990.gpkg', 'urb_gov.gpkg')
+# urb_gov = gpd.read_file('urb_gov.gpkg').to_crs('EPSG:4326')
+# os.remove('urb_gov.gpkg')
+# urb_joins = str(set(urb_gov['GISJOIN'])).replace('{','').replace('}','')
+#
+# conn = create_connection()
+# with conn:
+#     cur = conn.cursor()
+#     cur.execute(f"UPDATE zoning SET urbanized=1 WHERE gisjoin IN ({urb_joins})")
+#     conn.commit()
+
